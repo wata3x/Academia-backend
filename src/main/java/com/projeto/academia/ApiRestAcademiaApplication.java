565a18486f03e2c18c569c9c588dfbf4fd8037da
wata <felliperocha98@gmail.com>
2016-09-28 21:59:26 -0300

    Rejected commit: Default

---
+++ src/main/java/com/projeto/academia/ApiRestAcademiaApplication.java
@@ -0,0 +1,12 @@
+package com.projeto.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAcademiaApplication.class, args);
+	}
+}
